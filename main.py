a63c600d8c2667023b8fb6e2b750bbdf532f3984
sshubhm <shubhamsapra@hotmail.com>
2017-09-07 12:13:27 +0530

    a

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import numpy as np
-import cv2
+import cv2, thread, time
 from selenium import webdriver
 from pyvirtualdisplay import Display
-from showImgTest import showImg
-#creating hidden display
-display = Display(visible=0, size=(1920, 1080))
-display.start()
-print('display created')
+import os.path
+import os
 
-#opening webpage in display and saving the screenshot
-browser = webdriver.Firefox()
-browser.get('https://www.google.co.in/maps/@28.7195088,77.0684701,19.25z/data=!5m1!1e1')
-browser.save_screenshot('screenie1.png')
-browser.quit()
-#loading the image
+clear = lambda: os.system('clear')
 
-showImg()
+def fetchimg():
+    browser = webdriver.Firefox()
+    browser.get('https://www.google.co.in/maps/@28.7195088,77.0684701,19.25z/data=!5m1!1e1')
+    browser.save_screenshot('screenie.png')
+    browser.quit()
+
+
+def showimg():
+    while not os.access('screenie.png',os.W_OK):
+        print "fetching image"
+        time.sleep(0.5)
+        clear()
+        print "fetching image."
+        time.sleep(0.5)
+        clear()
+        print "fetching image.."
+        time.sleep(0.5)
+        clear()
+        print "fetching image..."
+        time.sleep(0.5)
+        clear()
+    if os.access('screenie.png', os.W_OK):
+        img = cv2.imread('screenie.png', 1)
+        cv2.imshow('image', img)
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+
+
+thread.start_new_thread(fetchimg, ())
+thread.start_new_thread(showimg,())
+while 1:
+    pass
