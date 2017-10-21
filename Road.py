13017e89777dc0b445ef9d4053e484e0164c40c3
sshubhm <shubhamsapra@hotmail.com>
2017-10-21 21:10:07 +0530

    Added browser window size

--- Road.py
+++ Road.py
@@ -1,7 +1,6 @@
 #class road
 
 class Road:
-
     def getColor(self):
         print self.color
 
--- crossing.py
+++ crossing.py
@@ -2,7 +2,6 @@
 import cv2
 from selenium import webdriver
 import numpy as np
-
 from Road import *
 
 def color(a):
@@ -18,19 +17,17 @@ def color(a):
         return a
 
 class crossing:
-
     #init method to set name and url of crossing
     def __init__(self,name, url):
         self.name = name
         self.url = url
-
     #getUpdate to get the latest image from Google Maps
     def getUpdate(self):
         browser = webdriver.Firefox()
+        browser.set_window_size(1366, 768)
         browser.get(self.url)
         browser.save_screenshot(self.name+".png")
         browser.quit()
-
     #fetch method to fetch the image
     def fetch(self):
         img = cv2.imread(self.name+".png")
@@ -46,7 +43,5 @@ class crossing:
         rTop.getColor()
         rRight.getColor()
         rBottom.getColor()
-
-
     def getName(self):
         print self.name
