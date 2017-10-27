587f9240ccf32f3a44ad4943cb563006d46604d3
sshubhm <shubhamsapra@hotmail.com>
2017-10-27 23:09:29 +0530

    Works for one cycle

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from TrafficLight import *
 from CrossingGUI import *
 
 cr1 = crossing("Maharaja",'https://www.google.co.in/maps/@28.7195088,77.0684701,19.25z/data=!5m1!1e1')
-#cr1.getUpdate()
+cr1.getUpdate()
 img = cr1.fetch()
 
 
@@ -35,6 +35,9 @@ rLeft.createLight(270,290,290,310,board.C)
 rTop.createLight(290,270,310,290,board.C)
 rRight.createLight(310, 290, 330, 310,board.C)
 rBottom.createLight(290, 310, 310, 330,board.C)
+
+
+#round robin scheduling
 roads = [rLeft, rTop, rRight, rBottom]
 
 for road in roads:
@@ -47,11 +50,23 @@ for road in roads:
         road.tLight.setTime(20)
     elif rColor == 'green':
         road.tLight.setTime(15)
-    print(rColor)
-board.top.after(rLeft.tLight.getTime()*1000,board.glow,rLeft.tLight,roads)
-board.top.after(1000*(rTop.tLight.getTime()+rLeft.tLight.getTime()),board.glow,rTop.tLight,roads)
-board.top.after(1000*(rTop.tLight.getTime()+rLeft.tLight.getTime()+rRight.tLight.getTime()),board.glow,rRight.tLight,roads)
-board.top.after(1000*(rLeft.tLight.getTime()+rTop.tLight.getTime()+rRight.tLight.getTime()+rBottom.tLight.getTime()),board.glow,rBottom.tLight,roads)
+
+# board.top.after(rLeft.tLight.getTime()*1000,board.glow,rLeft.tLight,roads)
+# board.top.after(1000*(rTop.tLight.getTime()+rLeft.tLight.getTime()),board.glow,rTop.tLight,roads)
+# board.top.after(1000*(rTop.tLight.getTime()+rLeft.tLight.getTime()+rRight.tLight.getTime()),board.glow,rRight.tLight,roads)
+# board.top.after(1000*(rLeft.tLight.getTime()+rTop.tLight.getTime()+rRight.tLight.getTime()+rBottom.tLight.getTime()),board.glow,rBottom.tLight,roads)
+
+
+initialDelay = 0
+for road in roads:
+    if road == rLeft:
+        board.glow(road.tLight,roads)
+        oldTime = road.tLight.getTime()
+        continue
+    print(initialDelay+oldTime)
+    board.top.after((initialDelay + oldTime)*1000, board.glow,road.tLight,roads)
+    oldTime = oldTime + road.tLight.getTime()
+
 
 
 board.top.mainloop()
